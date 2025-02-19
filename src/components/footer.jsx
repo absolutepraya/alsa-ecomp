5753a60c1ef7510a0b83395f7cecdf37b18c626a
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-19 16:08:54 +0700

    feat: replace text elements with GoldText component for improved styling in Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,19 @@
+import GoldText from './goldtext';
+
 export default function Footer() {
   return (
     <div className="bg-background flex w-full flex-col items-center gap-y-5 py-5 text-foreground">
-      <p className="text-2xl">Contact Person</p>
+      <GoldText
+        text="Contact Person"
+        className="text-3xl font-extrabold"
+      />
 
       <div className="flex w-full flex-row justify-center gap-x-6">
         <div className="flex w-[20rem] flex-col items-center gap-y-2">
-          <p className="text-xl">Sponsorship & In-Kind</p>
+          <GoldText
+            text="Sponsorship & In-Kind"
+            className="text-2xl font-extrabold"
+          />
           <div className="flex flex-col items-center gap-y-0 text-center text-sm">
             <p>Faza</p>
             <p>081311332449/LINE ID: fzfazila</p>
@@ -14,7 +22,10 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex w-[20rem] flex-col items-center gap-y-2">
-          <p className="text-xl">Registration</p>
+          <GoldText
+            text="Registration"
+            className="text-2xl font-extrabold"
+          />
           <div className="flex flex-col items-center gap-y-0 text-center text-sm">
             <p>Sherly</p>
             <p>082170460200/LINE ID: sherlyfll22</p>
@@ -23,7 +34,10 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex w-[20rem] flex-col items-center gap-y-2">
-          <p className="text-xl">Media Partner</p>
+          <GoldText
+            text="Media Partner"
+            className="text-2xl font-extrabold"
+          />
           <div className="flex flex-col items-center gap-y-0 text-center text-sm">
             <p>Nana</p>
             <p>081289867698/LINE ID: nnacwaa</p>
@@ -33,26 +47,51 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="flex w-full flex-row items-center justify-center gap-x-4 mt-2">
+      <div className="mt-2 flex w-full flex-row items-center justify-center gap-x-4">
         <div className="flex flex-row items-center gap-x-1 text-xs">
           <div className="h-7 w-7 rounded-full bg-gold" />
-          <a href="#">@alsaecomp</a>
+          <a
+            href="#"
+            className="underline decoration-[1px] underline-offset-[3px]"
+          >
+            @alsaecomp
+          </a>
         </div>
         <div className="flex flex-row items-center gap-x-1 text-xs">
           <div className="h-7 w-7 rounded-full bg-gold" />
-          <a href="#">ALSA LC UI</a>
+          <a
+            href="#"
+            className="underline decoration-[1px] underline-offset-[3px]"
+          >
+            ALSA LC UI
+          </a>
         </div>
         <div className="flex flex-row items-center gap-x-1 text-xs">
           <div className="h-7 w-7 rounded-full bg-gold" />
-          <a href="#">(alsa-twitter)</a>
+          <a
+            href="#"
+            className="underline decoration-[1px] underline-offset-[3px]"
+          >
+            (alsa-twitter)
+          </a>
         </div>
         <div className="flex flex-row items-center gap-x-1 text-xs">
           <div className="h-7 w-7 rounded-full bg-gold" />
-          <a href="#">@alsaecomp</a>
+          <a
+            href="#"
+            className="underline decoration-[1px] underline-offset-[3px]"
+          >
+            @alsaecomp
+          </a>
         </div>
         <div className="flex flex-row items-center gap-x-1 text-xs">
           <div className="h-7 w-7 rounded-full bg-gold" />
-          <a href="#">alsaecomp.com</a>
+          <a
+            href="#"
+            className="underline decoration-[1px] underline-offset-[3px]"
+          >
+            alsaecomp.com
+          </a>
         </div>
       </div>
     </div>

915cd5f6851de97b2d3bd266f08c80a84a8771ee
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-19 11:32:39 +0700

    feat: add Kaisar font family and update Tailwind CSS configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				kaisar: ['Kaisar', 'Arial', 'Helvetica', 'sans-serif'],
+			},
+			colors: {
+				primary: '#003140',
+				secondary: '#00576e',
+				tertiary: '#000910',
+				quaternary: '#294881',
+				foreground: '#c3deeb',
+				gold: '#d3c14b',
+				goldlight: '#f2e883',
+				golddark: '#ac9b42'
+			}
+		},
+	},
+	plugins: [],
 };

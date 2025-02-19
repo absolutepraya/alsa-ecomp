1ccd7c8aa9bfd78037928321b86d574f8c330b36
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-19 18:03:07 +0700

    feat: enhance NavBar component with active link styles for better user interaction

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,35 +11,35 @@ export default function NavBar({ currentRoute }) {
       <div className="flex items-center justify-between gap-x-12 text-lg">
         <a
           href="/"
-          className={`relative text-primary transition-transform hover:scale-105 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'home' ? 'after:scale-x-100' : ''}`}
+          className={`relative text-primary transition-transform hover:scale-105 active:scale-95 active:brightness-75 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'home' ? 'after:scale-x-100' : ''}`}
         >
           Home
         </a>
         <a
           href="/about"
-          className={`relative text-primary transition-transform hover:scale-105 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'about' ? 'after:scale-x-100' : ''}`}
+          className={`relative text-primary transition-transform hover:scale-105 active:scale-95 active:brightness-75 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'about' ? 'after:scale-x-100' : ''}`}
         >
           About Us
         </a>
         <a
           href="/competitions"
-          className={`relative text-primary transition-transform hover:scale-105 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'competitions' ? 'after:scale-x-100' : ''}`}
+          className={`relative text-primary transition-transform hover:scale-105 active:scale-95 active:brightness-75 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'competitions' ? 'after:scale-x-100' : ''}`}
         >
           Competitions
         </a>
         <a
           href="/timeline"
-          className={`relative text-primary transition-transform hover:scale-105 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'timeline' ? 'after:scale-x-100' : ''}`}
+          className={`relative text-primary transition-transform hover:scale-105 active:scale-95 active:brightness-75 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'timeline' ? 'after:scale-x-100' : ''}`}
         >
           Timeline
         </a>
         <a
           href="/partners"
-          className={`relative text-primary transition-transform hover:scale-105 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'partners' ? 'after:scale-x-100' : ''}`}
+          className={`relative text-primary transition-transform hover:scale-105 active:scale-95 active:brightness-75 after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-left after:scale-x-0 after:bg-primary after:transition-transform after:duration-300 hover:after:scale-x-100 ${currentRoute === 'partners' ? 'after:scale-x-100' : ''}`}
         >
           Partners
         </a>
-        <a href="registration" className="transition-transform hover:scale-105">
+        <a href="registration" className="transition-all active:scale-95 active:brightness-75 hover:scale-105">
           <Emboss innerClassName="px-3 py-0.5 text-lg rounded-full">Registration</Emboss>
         </a>
       </div>

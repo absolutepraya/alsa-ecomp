7bf36f6671bf46fb2523f7b955bcf946f31d953d
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-20 15:41:18 +0700

    feat: adjust home page height for improved layout consistency

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import BlurFade from '@/components/blurfade';
 
 export default function Home() {
   return (
-    <div className="relative flex min-h-[100lvh] flex-col items-center justify-center gap-y-20 overflow-x-clip px-6 lg:px-0">
+    <div className="relative flex h-[100lvh] flex-col items-center justify-center gap-y-20 overflow-x-clip px-6 lg:px-0">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <BlurFade
           delay={0.3}

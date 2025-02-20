aa6e75b791c25f9178cfdaf26792df24b9c0a3bf
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2025-02-20 15:12:59 +0700

    feat: update home page layout for better responsiveness and alignment

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import BlurFade from '@/components/blurfade';
 
 export default function Home() {
   return (
-    <div className="relative flex h-[100vh] flex-col items-center gap-y-20 overflow-x-clip lg:px-0 px-6">
+    <div className="relative flex min-h-[100dvh] flex-col items-center justify-center gap-y-20 overflow-x-clip px-6 lg:px-0">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <BlurFade
           delay={0.3}
